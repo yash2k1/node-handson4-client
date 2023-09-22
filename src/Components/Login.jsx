2d3f74e35a42c18de5da840eb31b780dc215ad5b
yash gupta <yashgupta1mole@gmail.com>
2023-09-22 20:46:12 +0530

    fina

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,112 +1,127 @@
 import React, { useEffect, useState } from "react";
-import img from "../assets/img.jpg";
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+// import img from "../assets/img.jpg";
 import "./Registration.css";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 const Login = () => {
-  const navigate=useNavigate();
-  const [formData,setFromData]=useState({
-    name:"",
-    phoneNo:"",
-    email:"",
-    password:""
-});
-const [submit,setSubmit]=useState(false);
-const [response,setResponse]=useState("");
-useEffect(()=>{
-    // const baseUrl="http://localhost:2023/Login"
-    const baseUrl="https://handson4-server.onrender.com/Login"
-    axios.post(baseUrl,formData).then((res)=>{
-     setResponse(res.data);
-     console.log(res.data)
-    })
-
-},[submit])
-const handleChange=(e)=>{
-    setFromData({...formData,[e.target.name]:e.target.value})
-}
-const handleClick=(e)=>{
-    e.preventDefault();
+  const navigate = useNavigate();
+  const [formData, setFromData] = useState({
+    email: "",
+    password: "",
+  });
+  const [submit, setSubmit] = useState(false);
+  const [response, setResponse] = useState("");
+  useEffect(() => {
+  //  const baseUrl="http://localhost:2023/Login"
+   const baseUrl="https://handson4-server.onrender.com/Login"
     
-    if(formData.name!=0 && formData.email!=0 &&formData.password!=0&&formData.phoneNo!=0){
+    axios.post(baseUrl, formData).then((res) => {
+      setResponse(res.data);
+      console.log(res.data);
+    });
+  }, [submit]);
+  const handleChange = (e) => {
+    setFromData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    if (
+      formData.email != 0 &&
+      formData.password != 0
+    ) {
       setSubmit(!submit);
-  }else{
-      setResponse({"message":"fill the form"})
+    
+      // goToDashboard();
+    } else {
+      setResponse({ message: "fill the form" });
+    }
+
+  };
+  const tostNotify=()=> toast("you are login in");
+  
+  response.token&&(function goToDashboard  (){
+    tostNotify();
+    setTimeout(()=>{
+      setFromData({
+        email: "",
+        password: "",
+      })
+      localStorage.setItem("token", response.token);
+      navigate("/Dashboard");
+      
+    },2000) 
   }
-}
-const goToDashboard=()=>{
-  console.log(response.token)
-  localStorage.setItem("token",response.token);  
-  navigate("/Dashboard");
-}
-
-  return (
+  )();
  
-    <div className='registration'>
+  return (
+    <>
+     
+    <div className="registration">
+     
+        <div className="registerContainer loginContainer">
+          <img className="registerImg" src="https://img.freepik.com/premium-vector/illustration-vector-graphic-cartoon-character-blogging_516790-1481.jpg?w=2000" alt="not found" />
+       
 
-   {  response.token?
-    goToDashboard():
-     <div className="registerContainer loginContainer">
-        <img src={img} alt="not found" />
-        <form className="register">
-          <h1>Login Page</h1>
-          <fieldset>
-            <legend>Name</legend>
-            {/* Name, Phone, Email and Password */}
-            <input
-              type="text"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              placeholder='Enter your Name'
-              required
-            />
-          </fieldset>
-          <fieldset>
-            <legend>Phone number</legend>
-            {/* Name, Phone, Email and Password */}
-            <input
-              type="text"
-              name="phoneNo"
-              value={formData.phoneNo}
-              onChange={handleChange}
-              placeholder='Enter your Phone number'
-              required
-            />
-          </fieldset>
-          <fieldset>
-            <legend>Email</legend>
-            {/* Name, Phone, Email and Password */}
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              placeholder='Enter your Email'
-              required
-            />
-          </fieldset>
-          <fieldset>
-            <legend>Password</legend>
-            {/* Name, Phone, Email and Password */}
-            <input
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              placeholder='Enter your Password'
-              required
-            />
-          </fieldset>
-          {/* <div className="message">Your are already registered</div> */}
-          <div className="message">{response&& response.message}</div>
-          <button onClick={handleClick}>Submit</button>
-          <div>
-            go to <Link to={"/Register"}>Register page</Link>
-          </div>
-        </form>
-      </div>}
-      </div>
+          <form className="register">
+            <h1 className="registerHeading">Login Page</h1>
+          
+        
+            <fieldset>
+              <legend>Email</legend>
+              {/* Email and Password */}
+              <EmailOutlinedIcon/>
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                placeholder="Enter your Email"
+                required
+              />
+            </fieldset>
+            <fieldset>
+              <legend>Password</legend>
+              {/* Name, Phone, Email and Password */}
+              <LockOutlinedIcon/>
+              <input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                placeholder="Enter your Password"
+                required
+              />
+            </fieldset>
+            {/* <div className="message">Your are already registered</div> */}
+            <div className="message">{response && response.message}</div>
+            <button className="registerButton " onClick={handleClick}>
+              Login 
+            </button>
+            <div className="goto">
+              go to <Link className="link" to={"/Register"}>Register page</Link>
+            </div>
+          </form>
+        </div>
+     
+    </div>
+    <ToastContainer 
+    position="top-right"
+autoClose={1000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="light" />
+    </>
+
   );
 };
 
--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,79 +1,155 @@
-import React, { useEffect, useState } from 'react'
-import img from "../assets/img.jpg"
-import './Registration.css'
-import { Link } from 'react-router-dom'
-import axios from "axios"
+import React, { useEffect, useState } from "react";
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import "./Registration.css";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 const Register = () => {
-    const [formData,setFromData]=useState({
-        name:"",
-        phoneNo:"",
-        email:"",
-        password:""
+  const navigate=useNavigate();
+  const [formData, setFromData] = useState({
+    name: "",
+    phoneNo: "",
+    email: "",
+    password: "",
+  });
+  const [submit, setSubmit] = useState(false);
+  const [response, setResponse] = useState("");
+  useEffect(() => {
+    const baseUrl="https://handson4-server.onrender.com/Register"
+    // const baseUrl="http://localhost:2023/Register"
+    axios.post(baseUrl, formData).then((res) => {
+      setResponse(res.data);
+      console.log(res.data);
     });
-    const [submit,setSubmit]=useState(false);
-    const [response,setResponse]=useState("");
-    useEffect(()=>{
-        const baseUrl="https://handson4-server.onrender.com/Register"
-        // const baseUrl="http://localhost:2023/Register"
-        axios.post(baseUrl,formData).then((res)=>{
-         setResponse(res.data);
-         console.log(res.data)
-        })
-        setFromData({
-            name:"",
-            phoneNo:"",
-            email:"",
-            password:""
-        });
-    },[submit])
-    const handleChange=(e)=>{
-        setFromData({...formData,[e.target.name]:e.target.value})
+    setFromData({
+      name: "",
+      phoneNo: "",
+      email: "",
+      password: "",
+    });
+  }, [submit]);
+  const handleChange = (e) => {
+    setFromData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (
+      formData.name != 0 &&
+      formData.email != 0 &&
+      formData.password != 0 &&
+      formData.phoneNo != 0
+    ) {
+      setSubmit(!submit);
+    } else {
+      setResponse({ message: "fill the form" });
     }
-    const handleClick=(e)=>{
-        e.preventDefault();
-        if(formData.name!=0 && formData.email!=0 &&formData.password!=0&&formData.phoneNo!=0){
-            setSubmit(!submit);
-        }else{
-            setResponse({"message":"fill the form"})
-
-        }
-   
-    }
+  };
+  const tostNotify=()=> toast("Registration successfull");
+  
+  response.token&&(function goToDashboard  (){
+    tostNotify();
+    setTimeout(()=>{
+      console.log("settimeout")
+      navigate("/Login");
+    },2000) 
+  }
+  )();
   return (
-    <div className='registration'>
-   <div className="registerContainer">
-    <img src={img} alt="not found" />
-    <form className='register'>
-    <h1>Registration Page</h1>
-        <fieldset>
-        <legend>Name</legend>
+    <>
+    
+    <div className="registration">
+      <div className="registerContainer">
+      <img className="registerImg" src="https://img.freepik.com/premium-vector/illustration-vector-graphic-cartoon-character-blogging_516790-1481.jpg?w=2000" alt="not found" />
+        
+        <form className="register">
+          <h1 className="registerHeading">Registration Page</h1>
+          <fieldset>
+            <legend>Name</legend>
             {/* Name, Phone, Email and Password */}
-            <input type="text" name="name" value={formData.name}  onChange={handleChange} placeholder='Enter your Name' required/>
-            </fieldset>
-        <fieldset>
-        <legend>Phone number</legend>
+            {/* <PersonIcon/> */}
+            <PersonOutlineOutlinedIcon/>
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              placeholder="Enter your Name"
+              required
+            />
+          </fieldset>
+          <fieldset>
+            <legend>Phone number</legend>
             {/* Name, Phone, Email and Password */}
-            <input type="text" name="phoneNo" value={formData.phoneNo}  onChange={handleChange} placeholder='Enter your Phone number' required/>
-            </fieldset>
-        <fieldset>
-        <legend>Email</legend>
+            {/* <LocalPhoneIcon/> */}
+            <LocalPhoneOutlinedIcon/>
+            <input
+              type="text"
+              name="phoneNo"
+              value={formData.phoneNo}
+              onChange={handleChange}
+              placeholder="Enter your Phone number"
+              required
+            />
+          </fieldset>
+          <fieldset>
+            <legend>Email</legend>
             {/* Name, Phone, Email and Password */}
-            <input type="email" name="email" value={formData.email}  onChange={handleChange} placeholder='Enter your Email' required />
-            </fieldset>
-        <fieldset>
-        <legend>Password</legend>
+            {/* <EmailIcon/> */}
+            <EmailOutlinedIcon/>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              placeholder="Enter your Email"
+              required
+              />
+          </fieldset>
+          <fieldset>
+            <legend>Password</legend>
             {/* Name, Phone, Email and Password */}
-            <input type="password" name="password" value={formData.password}  onChange={handleChange} placeholder='Enter your Password' required/>
-            </fieldset>
-            {/* <div className="message">Your are already registered</div> */}
-            <div className="message">{response && response.message}</div>
-            <button onClick={handleClick}>Submit</button>
-            <div>go to <Link to={"/Login"}>Login page</Link></div>
-    </form>
-   </div>
-   </div>
+            {/* <LockIcon/> */}
+            <LockOutlinedIcon/>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              placeholder="Enter your Password"
+              required
+            />
+          </fieldset>
+          {/* <div className="message">Your are already registered</div> */}
+          <div className="message">{response && response.message}</div>
+          <button className="registerButton" onClick={handleClick}>
+            Submit
+          </button>
+          <div className="goto" >
+            go to <Link className="link" to={"/Login"}>Login page</Link>
+          </div>
+        </form>
+      </div>
+    </div>
+        
+    <ToastContainer 
+    position="top-right"
+autoClose={1000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="light" />
+             
+              </>
 
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
